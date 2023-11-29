6c3f5c6dfab83652f39729b11d8e84743e003e7b
Ben Sloan <mrbensloan@gmail.com>
2023-11-29 14:46:31 +0000

    Added mysql package

--- app.js
+++ app.js
@@ -1,36 +1,46 @@
-const express = require('express');
-const morgan = require('morgan');
-const { exec } = require('child_process')
+const express = require("express");
+const morgan = require("morgan");
+const { exec } = require("child_process");
+
+// sql
+const mysql = require("mysql");
 
 // express app
 const app = express();
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "test",
+  password: "test",
+  database: "my_db",
+});
+connection.connect();
 
 // register view engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.urlencoded());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-app.listen(3000) // localhost:3000
+app.listen(3000); // localhost:3000
 
 // redirect index to customer pages
-app.get('/', (request, response) => {
-    response.redirect('/customer');
-})
+app.get("/", (request, response) => {
+  response.redirect("/customer");
+});
 
 // customer page
-app.get('/customer', (request, response) => {
-    response.render('customer', {
-        title: 'Customer View',
-        banner_text: 'Welcome, John Doe'
-    })
-})
+app.get("/customer", (request, response) => {
+  response.render("customer", {
+    title: "Customer View",
+    banner_text: "Welcome, John Doe",
+  });
+});
 
 // 404 page
 app.use((request, response) => {
-    response.status(404);
-    response.render('404', {
-        title: '404'
-    });
-})
+  response.status(404);
+  response.render("404", {
+    title: "404",
+  });
+});
