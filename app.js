1ce7377f5212a37b4697447ee36c2ca9c44b9725
Ben Sloan <mrbensloan@gmail.com>
2023-11-30 13:35:33 +0000

    Removed test sql query from app.js

--- app.js
+++ app.js
@@ -16,12 +16,6 @@ const connection = mysql.createConnection({
   acquireTimeout: 60000,
 });
 
-connection.query("SELECT 1 + 1 AS solution", (err, rows, fields) => {
-  if (err) throw err;
-
-  console.log("The solution is: ", rows[0].solution);
-});
-
 connection.end();
 
 // register view engine
@@ -34,58 +28,62 @@ app.use(express.json());
 
 app.listen(3000); // localhost:3000
 
-
-const crypto = require('crypto');
+const crypto = require("crypto");
 const generateSecretKey = () => {
-    return crypto.randomBytes(32).toString('hex');
-}
+  return crypto.randomBytes(32).toString("hex");
+};
 
 const secretKey = generateSecretKey();
 
-const session = require('express-session');
-app.use(session({
+const session = require("express-session");
+app.use(
+  session({
     secret: secretKey,
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: false }
-}))
-
+    cookie: { secure: false },
+  })
+);
 
 // handle login form submissions **validate logins against db here**
-app.post('/login-form', (request, response) => {
-    const formData = request.body; // email/password from login form
+app.post("/login-form", (request, response) => {
+  const formData = request.body; // email/password from login form
 
-    request.session.user = { type: 'customer', name: 'Steve Smith', email: 'test@email.com', phone: '0123456789', address: '123 Main Street, Dundee, ABC123' }; // test user to test sessions
-    response.redirect('/customer'); // redirect to page based on user type
-})
+  request.session.user = {
+    type: "customer",
+    name: "Steve Smith",
+    email: "test@email.com",
+    phone: "0123456789",
+    address: "123 Main Street, Dundee, ABC123",
+  }; // test user to test sessions
+  response.redirect("/customer"); // redirect to page based on user type
+});
 
 // destroy session on logout
-app.get('/logout', (request, response) => {
-    request.session.destroy((error) => {
-        if (error){
-            console.log(error);
-        }
-        response.redirect('/login');
-    })
-})
-
+app.get("/logout", (request, response) => {
+  request.session.destroy((error) => {
+    if (error) {
+      console.log(error);
+    }
+    response.redirect("/login");
+  });
+});
 
 // redirect index to main login page
 app.get("/", (request, response) => {
   response.redirect("/login");
-})
+});
 
 // login page
-app.get('/login', (request, response) => {
-    response.render('login', {
-        title: 'Login'
-    })
-})
+app.get("/login", (request, response) => {
+  response.render("login", {
+    title: "Login",
+  });
+});
 
-
-function isLoggedIn(type){
-    return (request, response, next) => {
-        /*if (request.session.user){
+function isLoggedIn(type) {
+  return (request, response, next) => {
+    /*if (request.session.user){
             if (request.session.user.type == type){
                 return next();
             } else {
@@ -94,54 +92,53 @@ function isLoggedIn(type){
         } else {
             response.redirect('/login');
         }*/
-        return next(); // remove when login implemented
-    }
+    return next(); // remove when login implemented
+  };
 }
 
-
 // customer page
-app.get('/customer', isLoggedIn('customer'), (request, response) => {
-    response.render('customer', {
-        title: 'Customer View',
-        banner_text: 'Welcome ' + request.session.user.name,
-        nav_title: 'Browse Products',
-        page: request.originalUrl,
-        filter: request.query.filter || 'price_desc',
-        customer_session: request.session.user
-    })
-})
+app.get("/customer", isLoggedIn("customer"), (request, response) => {
+  response.render("customer", {
+    title: "Customer View",
+    banner_text: "Welcome " + request.session.user.name,
+    nav_title: "Browse Products",
+    page: request.originalUrl,
+    filter: request.query.filter || "price_desc",
+    customer_session: request.session.user,
+  });
+});
 
 // customer details page
-app.get('/customer/details', isLoggedIn('customer'), (request, response) => {
-    response.render('customer_details', {
-        title: 'Your Details',
-        banner_text: 'Your Details',
-        nav_title: 'My Account',
-        page: request.originalUrl,
-        customer_session: request.session.user
-    })
-})
+app.get("/customer/details", isLoggedIn("customer"), (request, response) => {
+  response.render("customer_details", {
+    title: "Your Details",
+    banner_text: "Your Details",
+    nav_title: "My Account",
+    page: request.originalUrl,
+    customer_session: request.session.user,
+  });
+});
 
 // staff page
-app.get('/staff', isLoggedIn('staff'), (request, response) => {
-    response.render('staff', {
-        title: 'Staff View',
-        banner_text: 'Staff View',
-        nav_title: 'Inventory Management',
-        page: request.originalUrl,
-        customer_session: request.session.user
-    })
-})
+app.get("/staff", isLoggedIn("staff"), (request, response) => {
+  response.render("staff", {
+    title: "Staff View",
+    banner_text: "Staff View",
+    nav_title: "Inventory Management",
+    page: request.originalUrl,
+    customer_session: request.session.user,
+  });
+});
 
 // manager/performance page
 app.get("/manager/performance", (request, response) => {
-    response.render("performance", {
-      title: "Manager View",
-      banner_text: "Welcome, John Doe",
-    });
+  response.render("performance", {
+    title: "Manager View",
+    banner_text: "Welcome, John Doe",
   });
+});
 
-  // manager page
+// manager page
 app.get("/manager/manage-employees", (request, response) => {
   response.render("manage-employees", {
     title: "Manager View",
